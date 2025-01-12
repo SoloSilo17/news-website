6704cf36ba543c05d37811172ed3d07647ce0e62
SoloSilo17 <bryankeslergomez@gmail.com>
2025-01-12 13:51:45 -0500

    Added new code to Server.JS for static image uploads

--- server.js
+++ server.js
@@ -29,3 +29,6 @@ app.get('/', (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+app.use("/uploads", express.static("uploads"));
